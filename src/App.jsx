a00d8a132b22554bed168213f7c110b2416ad106
KEITA <ksekimotonz@gmail.com>
2023-05-11 23:11:56 +0900

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,58 @@
-import React from "react";
-import './App.css';
+import React, { useState } from "react";
+import ColorfulMessage from "./Components/ColorfulMessage";
 
 const App = () => {
-  return (
-    <div className="App">
+  console.log("first");
+  const [num, setNum] = useState(0)
+  const [faceShowFlag,setFaceShowFlag] = useState(true)
 
-    </div>
+  const ClickPlus = () => {
+    setNum(num + 1)
+  }
+  const ClickMinus = () => {
+    setNum(num - 1)
+  }
+  const ClickTimes = () => {
+    setNum(num * 2)
+  }
+  const ClickReset = () => {
+    setNum(0)
+  }
+
+  const onClickShowFlag = () => {
+    // !マークをつけると反対という意味になるので、true,falseには向いている
+    setFaceShowFlag(!faceShowFlag);
+  }
+
+  return (
+    <>
+
+      <ColorfulMessage
+        color="red"
+        message="Hello"
+        fontSize="50px"
+        />
+
+      <ColorfulMessage
+        color="blue"
+        message="World"
+        fontSize="10px"
+      />
+
+      <p>{num}</p>
+      <button onClick={ClickPlus}>+</button>
+      <button onClick={ClickMinus}>-</button>
+      <button onClick={ClickTimes}>x</button>
+      <button onClick={ClickReset}>Reset</button>
+      <br/>
+      <br/>
+
+      {/* &&の使い方を知る */}
+      {/* 左辺がtrueの場合、右辺が返される。 */}
+      {faceShowFlag && <p>( ´Д`)y━･~~</p>}
+      <button onClick={onClickShowFlag}>ON / OFF</button>
+
+    </>
   );
 }
 
---
+++ src/Components/ColorfulMessage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ColorfulMessage = (props) => {
+
+  // 毎回、props.ooと書くのは面倒なので、
+  // 分割代入でpropsを渡してあげることで、
+  // props名だけで使用できる
+  const {color,fontSize} = props
+
+  const contentStyle = {
+    // color: color,の時は省略できる
+    color,
+    fontSize
+  }
+
+  return (
+    <>
+      <h1 style={contentStyle}>
+        {props.message}
+      </h1>
+    </>
+  )
+}
+
+export default ColorfulMessage;
